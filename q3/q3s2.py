c456cbadaafa1b18eddbb5736232078716d81c8d
Oleg Paska <olegpaska2005@gmail.com>
2025-12-03 17:53:38 +0000

    q3s2

--- q3/q3s2.py
+++ q3/q3s2.py
@@ -1,15 +1,22 @@
 
+def maxi(arr, prev):
+    max = [0,0]
+    for i in range(prev, len(arr)):
+        if int(arr[i]) > int(max[0]):
+            max = [arr[i], i+1]
+    return max
+
 
 total = 0
 digits = [0] * 12
 with open("q3/in.txt") as f:
     for l in f:
         li = list(l.strip())
-        digits[0] = max(li[:-11])
+        digits[0], prev = maxi(li[:-11], 0)
         for i in range(1,12):
             o = 11 - i
-            digits[i] = max(li[digits.index(digits[i-1])+1:-o])
-
+            digits[i], prev = maxi(li[:len(li)-o], prev)
+            
         jolts = int("".join(digits))
         total += jolts
         print(jolts)
