43081fc6cfcb457b8a91b2ec96117692cc490902
Oleg Paska <olegpaska2005@gmail.com>
2025-12-06 09:11:55 +0000

    q5s2

--- q5/q5s2.py
+++ q5/q5s2.py
@@ -6,33 +6,36 @@ with open("q5/in.txt") as f:
         if l == "\n": break
         ranges.append(l.strip())
 
-
+delete = set()
 for i in range(len(ranges)):
     changes = True
-    delete = set()
     while changes == True:
         changes = False
         for j in range(len(ranges)):
             if i == j: continue
+            if j in delete or i in delete: continue
             r1 = list(map(int, ranges[i].split('-')))
             r2 = list(map(int, ranges[j].split('-')))
             if r2[0] in range(r1[0], r1[1]+1):
                 if r2[1] > r1[1]:
                     ranges[i] = f"{r1[0]}-{r2[1]}"
                 delete.add(j)
-                changes == True
+                changes = True
             if r2[1] in range(r1[0], r1[1]+1):
                 if r2[0] < r1[0]:
                     ranges[i] = f"{r2[0]}-{r1[1]}"
                 delete.add(j)
-                changes == True
+                changes = True
 
-ranges = [x for x in ranges if x not in delete]
+#ranges = [x for x in ranges if x not in delete]
+
+for i in sorted(delete, reverse=True):
+    ranges.pop(i)
 
 res = 0
 for i in ranges:
     r = list(map(int, i.split('-')))
-    res += r[1] - r[0]
+    res += r[1]+1 - r[0]
 
 print(ranges)
 
